159e6fd15e242ed486fa962f1b7a1205ab10bd2a
David Rex <davidhilmerrex@gmail.com>
2012-10-18 10:57:31 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yearsweb::Application.config.secret_token = '786f630d990258a6f55e3ac3520af3744289f137fec4d68503a22061a7c9a7ed745abc9c6f200bd26aca7ee0278e709e72879460bb77e4ae3d01a74806a16f7d'
